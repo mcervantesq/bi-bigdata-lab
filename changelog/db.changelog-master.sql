b203c6ab0c5e9cff2d400c3c25cce3c75a7e3ba5
Marco Orlando Cervantes Quispe <mcervantesq@ucvvirtual.edu.pe>
2026-09-18 20:31:27 -0500

    feat: add staging schema changelog

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -10,3 +10,9 @@ COMMENT ON SCHEMA workspace.bi_lab_70044650 IS
 'Laboratorio 02 - BI y Big Data - UCV';
 
 --rollback COMMENT ON SCHEMA workspace.bi_lab_70044650 IS NULL;
+
+--changeset estudiante:003
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_70044650
+COMMENT 'Staging schema - BI and Big Data - Lab 03';
+
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_70044650;
